33454a05d157c610d6aa98235d0e1f6704593d86
revolk <finalrevolk@gmail.com>
2024-07-30 12:36:39 +0000

    update

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 from collections import deque
 
+
 def canUnlockAll(boxes):
     n = len(boxes)
     unlocked = [False] * n
-    queue = deque([0])  # نبدأ بالصندوق الأول الذي يكون مفتوحاً
+    queue = deque([0])  # بدء قائمة الانتظار بالبوكس الأول
     unlocked[0] = True
-    
+
     while queue:
         current_box = queue.popleft()
         for key in boxes[current_box]:
             if not unlocked[key] and key < n:
                 unlocked[key] = True
                 queue.append(key)
-    
+
     return all(unlocked)
